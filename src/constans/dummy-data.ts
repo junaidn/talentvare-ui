5ed7fef6d7b87dd9d3a7521f1abb65574120969f
junaidn <junaidnaseer137@gmail.com>
2024-12-07 19:19:16 +0500

    separate dummy data

---
+++ src/constans/dummy-data.ts
@@ -0,0 +1,257 @@
+import { DropdownOptions } from "@/components/dropdown/Dropdown";
+import { JobPosting } from "@/components/job-card/JobCard";
+import { UpcomingInterviewProps } from "@/components/upcoming-interview/UpcomingInterview";
+import { teams } from "./images";
+
+export const jobPostings: {
+    featured: JobPosting[];
+    recommended: JobPosting[];
+    latest: JobPosting[];
+} = {
+    featured: [
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+    ],
+    recommended: [
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+    ],
+    latest: [
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+        {
+            companyImage: teams,
+            companyName: "Teams",
+            jobTitle: "UI/UX Designer",
+            jobLocation: "Seattle, USA (Remote)",
+            postingTime: "1 day ago",
+            noOfApplicants: 22,
+        },
+    ],
+};
+
+export const upcomingInterviews: UpcomingInterviewProps[] = [
+    {
+        jobTitle: "UI UX Designer",
+        jobLocation: "Remote",
+        companyImage: teams,
+        companyName: "Figma",
+        date: "16th Feb",
+        time: "13:45",
+    },
+    {
+        jobTitle: "UI UX Designer",
+        jobLocation: "Remote",
+        companyImage: teams,
+        companyName: "Figma",
+        date: "16th Feb",
+        time: "13:45",
+    },
+    {
+        jobTitle: "UI UX Designer",
+        jobLocation: "Remote",
+        companyImage: teams,
+        companyName: "Figma",
+        date: "16th Feb",
+        time: "13:45",
+    },
+];
+
+export const locations: DropdownOptions = [
+    { value: "pakistan", label: "Pakistan" },
+    { value: "us", label: "United States" },
+    { value: "germany", label: "Germany" },
+    { value: "dubai", label: "Dubai" },
+];
+
+export const jobTypes: DropdownOptions = [
+    { value: "fulltime", label: "Full Time" },
+    { value: "parttime", label: "Part Time" },
+    { value: "remote", label: "Remote" },
+];
